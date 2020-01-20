3e1f09ea2e6bf05053bd81339e63241d029979a7
Mastercpp <s8peliii@stud.uni-saarland.de>
2020-01-20 19:59:31 +0100

    new

--- exec.sh
+++ exec.sh
@@ -5,7 +5,7 @@ module add mpi/intel/2018
 echo "staring.."
 date
 
-mpicxx $HOME/MPI/MPI_pro/matrix.cpp -o $HOME/MPI/MPI_pro/matrix -lm -w
-mpirun $HOME/MPI/MPI_pro/matrix
+mpicxx $HOME/MPI/MPI_pro/matrix_mul_v1.cpp -o $HOME/MPI/MPI_pro/matrix_mul_v1 -lm -w
+mpirun $HOME/MPI/MPI_pro/matrix_mul_v1
 
 date
